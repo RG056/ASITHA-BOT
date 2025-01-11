805013e7dad5fe6032a40200ecc75f867a49c9f0
RG056 <155914752+RG056@users.noreply.github.com>
2025-01-12 02:44:19 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,"𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=22BCCYpD#UWTTsJidp6AcqX6bVSgikJmAwLXFiiNKanasOgw1Oz4"
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=22BCCYpD#UWTTsJidp6AcqX6bVSgikJmAwLXFiiNKanasOgw1Oz4' : process.env.SESSION_ID,
 
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
